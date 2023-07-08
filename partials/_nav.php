7b3ea4dd26e2a834150fffdfeb4ad5daa8f28048
ysaxena820 <54067233+ysaxena820@users.noreply.github.com>
2023-07-08 22:26:30 +0530

    Add files via upload

---
+++ partials/_nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/Self_Project">DataLeakageDetection</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/Self_Project/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Self_Project/login.php">User Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Self_Project/adminlogin.php">Admin Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Self_Project/Signup.php">Sign Up</a>
+        </li>
+        
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
